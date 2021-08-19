29839b5b9ab3da7688cf2421f29f0e42b35eea62
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-19 17:52:00 -0500

    :sparkles: Update

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+/** Useful to have everything on one place */
+
+const LSP_NAME = 'cl-lsp';
+
+export { LSP_NAME };
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,39 @@
+import {
+  workspace,
+  ExtensionContext,
+  commands,
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  Executable,
+} from 'coc.nvim';
 
+let languageClient: LanguageClient;
 
+export function activate(context: ExtensionContext) {
+  const config = workspace.getConfiguration('cl-lsp');
+
+  const cl: Executable = {
+    command: config.get<string>('serverPath', 'cl-lsp'),
+    args: config.get<string[]>('serverArguments', []),
+  };
+
+  const toolchain = config.get<string>('toolchainPath', '');
+  if (toolchain) {
+    cl.options = { env: { ...process.env, CL_LSP_TOOLCHAIN_PATH: toolchain } };
+  }
+
+  const serverOptions: ServerOptions = cl;
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: ['lisp'],
+  };
+
+  languageClient = new LanguageClient('cl-lsp', 'Common Lisp Language Server', serverOptions, clientOptions);
+
+  context.subscriptions.push(languageClient.start());
+
+  context.subscriptions.push(
+    commands.registerCommand('lisp.interrupt', () => languageClient.sendNotification('lisp/interrupt', {}))
+  );
+}
